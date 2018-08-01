c3ac6a8f7c91643afe2b35d0f682e78260265317
Nicholas Melhado <n.melhado@gmail.com>
2018-07-31 20:36:10 -0400

    startup

---
+++ test/userRoles.spec.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const userRoles = require('../userRoles');
+
+describe('userRoles', () => {
+  it('exists', () => {
+    expect(userRoles).to.be.ok;
+  })
+})
